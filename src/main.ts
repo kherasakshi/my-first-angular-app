75346510c0be6279fa109b2bc4e32908018ab281
kherasakshi <sakshikhera456@gmail.com>
2025-09-04 19:16:53 -0400

    main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter, Route } from '@angular/router';
+import { provideRouter, Route, withHashLocation } from '@angular/router';
 import { HomePageComponent } from './app/course-project/home-page/home-page.component';
-
 
 const routes: Route[] = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  // Add more routes here if needed
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes, withHashLocation())]
 });
